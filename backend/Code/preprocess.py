d913db467248f1defaec235c6be26a6456eb4765
NaNa <getingup10@gmail.com>
2024-05-18 15:13:26 +0700

    fixbug

--- backend/Code/preprocess.py
+++ backend/Code/preprocess.py
@@ -1,14 +1,15 @@
+import numpy as np
 import pandas as pd
 import pickle
 import re
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import py_vncorenlp
 
-def vncore(data):
+vncore = py_vncorenlp.VnCoreNLP(save_dir="../Model", annotators=["wseg"], max_heap_size='-Xmx500m')
+
+def vncore_process(data):
     #py_vncorenlp.download_model(save_dir='./Model') #install py_vncorenlp
-    vncore = py_vncorenlp.VnCoreNLP(save_dir="../Model", annotators=["wseg"], max_heap_size='-Xmx500m')
-    data= data.apply(lambda x: ' '.join(vncore.word_segment(x)))
-    return data
+    return vncore.word_segment(data)
 
 def transform_text(text, shortwords_dict):
     # Pattern to match URLs
@@ -28,22 +29,28 @@ def transform_text(text, shortwords_dict):
 def preprocess(data):
     # Process data
     shortwords_dict = pd.read_csv("../Data/vietnamese_shortwords.csv", sep=';', header=None, index_col=0).squeeze().to_dict()
-    data = data.str.lower().apply(transform_text, args=(shortwords_dict,))
-    data = vncore(data)
+    if isinstance(data, pd.Series):
+        data = data.apply(lambda x: transform_text(str(x).lower(), shortwords_dict))
+        data = data.apply(lambda x: vncore_process(x))
+        data = data.apply(' '.join)
+    else:  # If the input is a single text entry
+        data = transform_text(data.lower(), shortwords_dict)
+        data = vncore_process(data)
+        data = ' '.join(data)
     return data
 
-def countVectorrizer(data):
+def countVectorizer(data):
     #Load precessed_data
-    processed_data = preprocess(data)
+    processed_data = np.array(preprocess(data))
     # Load stopwords
     stopwords = pd.read_csv("../Data/vietnamese_stopwords.csv", header=None).squeeze().tolist()
 
     # Vectorize text
     vectorizer = CountVectorizer(stop_words=stopwords)
     data_vectorized = vectorizer.fit_transform(processed_data)
-    return processed_data
+    return data_vectorized
     
-def tfidfVectorize(data):
+def tfidfVectorizer(data):
     #Load precessed_data
     processed_data = preprocess(data)
     # Load stopwords
@@ -52,4 +59,4 @@ def tfidfVectorize(data):
     # Vectorize text
     vectorizer = TfidfVectorizer(stop_words=stopwords)
     data_vectorized = vectorizer.fit_transform(processed_data)
-    return processed_data
+    return data_vectorized
