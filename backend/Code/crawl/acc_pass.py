0c6088d4be025863ff7af812959f7785e36e9415
Dương Ngọc Thái <101975549+NT1610@users.noreply.github.com>
2025-09-05 09:09:10 +0700

    Update acc_pass.py

--- backend/Code/crawl/acc_pass.py
+++ backend/Code/crawl/acc_pass.py
@@ -1,9 +1,9 @@
 ## Facebook info
-ACCOUNT = "ngocthaint1610@gmail.com"  # your account
-PASS = "thai1610"  # your password
+ACCOUNT = ""  # your account
+PASS = ""  # your password
 
 # USER_DATA_DIR = "C:\\Users\\admin\\AppData\\Local\\Google\\Chrome\\User Data\\"  # path to chrome user data
-USER_DATA_DIR = "D:\\Python"  # path to chrome user data
+USER_DATA_DIR = ""  # path to chrome user data
 ## C:\\Users\\admin\\AppData\\Local\\Google\\Chrome\\User Data\\
 ## Set up database
 HOST_DB = "localhost"  # database host (example: localhost)
